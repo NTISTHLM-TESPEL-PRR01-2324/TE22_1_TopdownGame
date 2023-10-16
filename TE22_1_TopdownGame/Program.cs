6b539ed98064101a61e151ffa37732e470db5e4a
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2023-10-16 09:54:01 +0200

    Scener

--- TE22_1_TopdownGame/Program.cs
+++ TE22_1_TopdownGame/Program.cs
@@ -7,34 +7,73 @@ Raylib.SetTargetFPS(60);
 
 // int x = 0;
 // Vector2 position = new Vector2(0, 0);
-// Vector2 movement = new Vector2(2, 1);
+Vector2 movement = new Vector2(0, 0);
 
 Color hotPink = new Color(255, 105, 180, 255);
 
 Rectangle characterRect = new Rectangle(300, 400, 64, 64);
 Texture2D characterImage = Raylib.LoadTexture("gubbe.png");
 
+float speed = 5;
+
+string scene = "start";
+
 while (!Raylib.WindowShouldClose())
 {
-  // position += movement;
-
-  if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+  if (scene == "game")
   {
 
+    // position += movement;
+    movement = Vector2.Zero;
+
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_UP))
+    {
+      movement.Y = -1;
+    }
+    else if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN))
+    {
+      movement.Y = 1;
+    }
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+    {
+      movement.X = -1;
+    }
+    else if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+    {
+      movement.X = 1;
+    }
+
+    if (movement.Length() > 0)
+    {
+      movement = Vector2.Normalize(movement) * speed;
+    }
+    characterRect.x += movement.X;
+    characterRect.y += movement.Y;
   }
-
-  // characterRect.x += movement.X;
-  // characterRect.y += movement.Y;
-
+  else if (scene == "start")
+  {
+    if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
+    {
+      scene = "game";
+    }
+  }
   // x++;
   Raylib.BeginDrawing();
-  Raylib.ClearBackground(Color.GOLD);
+  if (scene == "game")
+  {
+    Raylib.ClearBackground(Color.GOLD);
 
-  Raylib.DrawTexture(characterImage, (int) characterRect.x, (int) characterRect.y, Color.WHITE);
+    Raylib.DrawTexture(characterImage, (int)characterRect.x, (int)characterRect.y, Color.WHITE);
+  }
+  else if (scene == "start")
+  {
+    Raylib.ClearBackground(Color.BLUE);
+    Raylib.DrawText("PRESS SPACE TO START", 10, 10, 32, Color.WHITE);
+  }
 
   // Raylib.DrawCircleV(position, 50, hotPink);
   // Raylib.DrawCircle(x, 300, 50, hotPink);
-  
+
   // Raylib.DrawRectangle(10, 40, 300, 50, Color.BLACK);
   // Raylib.DrawRectangleRec(wall, Color.WHITE);
 
